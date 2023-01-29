04c660f275bbec4419c00c795e28ea2692a5ffe3
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2023-01-29 01:20:20 -0300

    Documentação automatizada

---
+++ documentation/html/search/all_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['release_1',['release',['../namespaceconf.html#aca7e23e5f354ec5d8e18d6b5769908c9',1,'conf']]]
+];
---
+++ documentation/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghkmprtwy",
+  1: "fkmpwy",
+  2: "cdmp",
+  3: "_cfkmprwy",
+  4: "_cgp",
+  5: "abcdefhmprt",
+  6: "ct"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Arquivos",
+  4: "Funções",
+  5: "Variáveis",
+  6: "Páginas"
+};
+
